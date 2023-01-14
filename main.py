3048f593668fb6a21df1b843df3c4bbdefc4c058
Necroplod <necroplod@mail.ru>
2023-01-14 20:31:10 +0300

    create handlers

--- main.py
+++ main.py
@@ -10,8 +10,10 @@ class Setup(commands.Bot):
 
 		self.EXTENSIONS = [
 			'cogs.dev',
-			'cogs.events',
 			'cogs.suggest'
+		]
+		self.HANDLERS = [
+			'cogs.event'
 		]
 	
 	async def setup_hook(self):
@@ -24,11 +26,21 @@ class Setup(commands.Bot):
 				print(f'[!] {e}\n{self.line}')
 			else:
 				print(f'[!] Модуль {extension} успешно загружен.')
+		print(self.line)
+		for handler in self.HANDLERS:
+			try:
+				await self.load_extension(handler)
+			except Exception as e:
+				print(f'{self.line}\n[!] Не удалось загрузить хендлер {handler}.')
+				print(f'[!] {e}\n{self.line}')
+			else:
+				print(f'[!] Хендлер {handler} успешно загружен.')
 
 
 client = Setup(
 	status = discord.Status.idle,
-	case_insensitive = True, 
+	case_insensitive = True,
+	activity = discord.Streaming(name=f'g.help', url='https://youtu.be/dQw4w9WgXcQ'),
 	command_prefix = config.PREFIX, 
 	intents = Intents.all())
 client.run(config.TOKEN)
